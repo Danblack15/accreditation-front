8ae4d38ce2da4a60834735bf94fc6cfa99ec0dbc
Даниил Федоров <fedorov_danilka@bk.ru>
2023-10-28 20:32:11 +0500

    add test POST

--- store/items.js
+++ store/items.js
@@ -1,14 +1,14 @@
 export default {
-    state: () => ({
-        allItems: [],
+	state: () => ({
+		allItems: [],
 		allData: null,
 		isLoading: false
-    }),
+	}),
 
-    getters: {
-        getAllitems(state) {
-            return state.allItems;
-        },
+	getters: {
+		getAllitems(state) {
+			return state.allItems;
+		},
 
 		getLoading(state) {
 			return state.isLoading;
@@ -17,12 +17,12 @@ export default {
 		getAllData(state) {
 			return state.allData;
 		}
-    },
+	},
 
-    mutations: {
-        setNewItem(state, newItem) {
-            state.allItems.push(newItem);
-        },
+	mutations: {
+		setNewItem(state, newItem) {
+			state.allItems.push(newItem);
+		},
 
 		toggleLoading(state, value) {
 			state.isLoading = value;
@@ -31,24 +31,36 @@ export default {
 		setAllData(state, data) {
 			state.allData = data;
 		}
-    },
+	},
 
-    actions: {
-        addNewItem({ commit }, newItem) {
-            commit('setNewItem', newItem);
-        },
+	actions: {
+		addNewItem({ commit }, newItem) {
+			commit('setNewItem', newItem);
+		},
 
 		async fetchData({ commit, state }) {
 			if (state.allData) return;
-			
+
 			commit('toggleLoading', true);
+			let response = await fetch('https://3153-46-146-224-1.ngrok-free.app/api', {
+				method: "POST",
+				mode: "no-cors",
+				headers: {
+					'ngrok-skip-browser-warning': '1'
+				},
+				body: {value: 'Hello, its Danchik'}
+			});
+			console.log(response);
+			// this.$axios.setHeader('ngrok-skip-browser-warning', 1)
+			// const data = await this.$axios.get('https://3ddf-46-146-224-1.ngrok-free.app/api');
 
-			const { data } = await this.$axios.get('http://ai.vp-pspu.cf/');
-			console.log(data);
+			// if (data) {
+			// 	console.log(data);
 
-			commit('setAllData', data);
-			commit('toggleLoading', false);
+			// 	commit('setAllData', data);
+			// }
+			// commit('toggleLoading', false);
 		}
-    }
+	}
 
 }
